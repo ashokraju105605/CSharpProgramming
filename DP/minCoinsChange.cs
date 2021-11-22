7ec187ef61550eeda644d289fc287f1218b6c8ec
ashokraju105605 <60311993+ashokraju105605@users.noreply.github.com>
2021-11-22 17:27:41 +0530

    add the recursion changes for mincoinschange problem

--- DP/minCoinsChange.cs
+++ DP/minCoinsChange.cs
@@ -10,6 +10,8 @@ class minCoinsChange
         int V = 11;
         Console.Write("Minimum coins required is " +
                              minCoins(coins, V));
+        Console.Write("Minimum coins required is " +
+                             minCoinsRec(coins, V));
     }
     static int minCoins(int[] coins, int V)
     {
@@ -25,5 +27,32 @@ class minCoinsChange
             }
         }
         return table[table.Length - 1];
+    }
+    static int minCoinsRec(int[] coins, int V)
+    {
+
+        // base case
+        if (V == 0) return 0;
+
+        // Initialize result
+        int res = int.MaxValue;
+
+        // Try every coin that has
+        // smaller value than V
+        for (int i = 0; i < coins.Length; i++)
+        {
+            if (coins[i] <= V)
+            {
+                int sub_res = minCoins(coins, V - coins[i]);
+
+                // Check for INT_MAX to
+                // avoid overflow and see
+                // if result can minimized
+                if (sub_res != int.MaxValue &&
+                            sub_res + 1 < res)
+                    res = sub_res + 1;
+            }
+        }
+        return res;
     }
 }
