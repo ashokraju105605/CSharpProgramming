4358f0359ffbe021d968fc99e7ff3a4ac6c8f4d3
ashokraju105605 <60311993+ashokraju105605@users.noreply.github.com>
2021-12-01 20:40:50 +0530

    add changes for palindrom using a func

--- Strings/LongestPalindSubstr.cs
+++ Strings/LongestPalindSubstr.cs
@@ -1,12 +1,13 @@
 using System;
 class LongestPalindSubstr
 {
-    public static void main(string[] args)
+    public static void Main(string[] args)
     {
         string str = "forgeeksskeegfor";
         Console.WriteLine(str);
         LongestPalindSubstr lpss = new LongestPalindSubstr();
         lpss.printLongPalindSubstr(str);
+        lpss.printLongPalindSubstrRec(str);
 
     }
     void printLongPalindSubstr(string str)
@@ -43,6 +44,49 @@ class LongestPalindSubstr
         }
         Console.WriteLine("maximum palindrome length is : " + maxpalinlength);
         Console.WriteLine("palindrome is  : " + str.Substring(start, maxpalinlength));
+    }
+    void printLongPalindSubstrRec(string str)
+    {
+        int maxpalinlength = 1;
+        int j, k;
+        int start = 0;
+        var ret = new Tuple<int,int>(0,0);
+        for (int i = 0; i < str.Length; i++)
+        {
+            j = i; k = i + 1;
+            ret = maxPal(str,j,k);
+            if(ret.Item2>maxpalinlength && ret.Item1!=-1)
+            {
+                start = ret.Item1;
+                maxpalinlength = ret.Item2;
+            }
+            j = i - 1; k = i + 1;
+            ret = maxPal(str,j,k);
+            if(ret.Item2>maxpalinlength && ret.Item1!=-1)
+            {
+                start = ret.Item1;
+                maxpalinlength = ret.Item2;
+            }
+
+        }
+        Console.WriteLine("maximum palindrome length is : " + maxpalinlength);
+        Console.WriteLine("palindrome is  : " + str.Substring(start, maxpalinlength));
+    }
+    Tuple<int,int> maxPal(string str, int i, int j)
+    {
+        int maxpalinlength = 0;
+        int start = -1;
+        while (i >= 0 && j < str.Length && str[i] == str[j])
+        {
+            if (j - i > maxpalinlength)
+            {
+                start = i;
+                maxpalinlength = j - i + 1;
+            }
+            i--; j++;
+
+        }
+        return Tuple.Create(start,maxpalinlength);
     }
 }
 
