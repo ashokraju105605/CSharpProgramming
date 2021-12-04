812cb097e4b1dc9432573dac6b363c86cde90adc
ashokraju105605 <60311993+ashokraju105605@users.noreply.github.com>
2021-12-05 00:20:10 +0530

    add memoization technique for max A's problem

--- Recursion/printMaxAs.cs
+++ Recursion/printMaxAs.cs
@@ -1,13 +1,16 @@
 using System;
 class printmaxAs
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
+        int[] dp = new int[25];
+        Array.Fill(dp, -1);
         Console.WriteLine("Jai Shree Ram");
         for (int i = 1; i <= 20; i++)
         {
             Console.WriteLine("Max A's that can be printed is: " + findOptimal(i));
             Console.WriteLine("Max A's that can be printed is: " + findOptimalDP(i));
+            Console.WriteLine("Max A's that can be printed is: " + findOptimalMem(i, dp));
         }
     }
     static int findOptimal(int n)
@@ -33,6 +36,16 @@ class printmaxAs
             solution[i - 1] = Math.Max(Math.Max(2 * solution[i - 4], 3 * solution[i - 5]), 4 * solution[i - 6]);
 
         return solution[n - 1];
+    }
+    static int findOptimalMem(int n, int[] dp)
+    {
+        if (dp[n] != -1)
+            return dp[n];
+        if (n <= 6)
+        {
+            return dp[n] = n;
+        }
+        return dp[n] = Math.Max(Math.Max(2 * findOptimal(n - 3), 3 * findOptimal(n - 4)), 4 * findOptimal(n - 5));
     }
 }
 
